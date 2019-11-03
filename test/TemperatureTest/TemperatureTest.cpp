4df74a577815795391f38109f7a3fc10a99191de
Javier R.Uria <jrusuarez@gmail.com>
2019-11-03 21:56:11 +0000

    Solución ejercicio2 con fff

--- test/TemperatureTest/TemperatureTest.cpp
+++ test/TemperatureTest/TemperatureTest.cpp
@@ -1,21 +1,39 @@
 #include "CppUTest/TestHarness.h"
 #include "Temperature/Temperature.h"
+#include "HAL/ADC.h"
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS;
+FAKE_VALUE_FUNC(uint16_t, getADCValue);
 
 TEST_GROUP(TemperatureTestGroup)
 {
+  void setup() {
+    RESET_FAKE(getADCValue);
+  }
+
+  void teardown() {
+     // Uninit stuff
+  }
 };
 
 TEST(TemperatureTestGroup, 0Degrees)
 {
-    FAIL("Test not implemented yet");
+    getADCValue_fake.return_val = 0;
+    CHECK_EQUAL(0, getTemperatureInCx10());
+    CHECK_EQUAL(1, getADCValue_fake.call_count);
 }
 
 TEST(TemperatureTestGroup, 25Degrees)
 {
-    FAIL("Test not implemented yet");
+    getADCValue_fake.return_val = ADC_MAX_VALUE / 4;
+    CHECK_EQUAL(250, getTemperatureInCx10());
+    CHECK_EQUAL(1, getADCValue_fake.call_count);
 }
 
 TEST(TemperatureTestGroup, 100Degrees)
 {
-    FAIL("Test not implemented yet");
+    getADCValue_fake.return_val = ADC_MAX_VALUE;
+    CHECK_EQUAL(1000, getTemperatureInCx10());
+    CHECK_EQUAL(1, getADCValue_fake.call_count);
 }
